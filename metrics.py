801cda15f8982dfeac2beb7e50c2d304061e8888
hehapkomah7 <152416381+hehapkomah7@users.noreply.github.com>
2023-12-10 13:31:56 +0700

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -20,26 +20,13 @@ def binary_classification_metrics(prediction, ground_truth):
     accuracy = np.sum(prediction == ground_truth) / prediction.size
     f1 = TP / (TP + 0.5 * (FP + FN))
 
-    # TODO: implement metrics!
-    # Some helpful links:
-    # https://en.wikipedia.org/wiki/Precision_and_recall
-    # https://en.wikipedia.org/wiki/F1_score
 
     return precision, recall, f1, accuracy
 
 
 def multiclass_accuracy(prediction, ground_truth):
-    '''
-    Computes metrics for multiclass classification
+   
 
-    Arguments:
-    prediction, np array of int (num_samples) - model predictions
-    ground_truth, np array of int (num_samples) - true labels
-
-    Returns:
-    accuracy - ratio of accurate predictions to total samples
-    '''
-    # TODO: Implement computing accuracy
     correct_samples = 0
     for i in range(prediction.shape[0]):
         if ground_truth[i] == prediction[i]:
